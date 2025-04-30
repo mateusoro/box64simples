0fee85ba95d3f590a03c4261ea89521bd1079929
mateusoro <mateusoro@gmail.com>
2025-04-29 21:25:59 -0300

    sasas

--- instalador.sh
+++ instalador.sh
@@ -301,9 +301,13 @@ cd "/sdcard/Download/Jogos Winlator/Borderlands Game of the Year Enhanced/Binari
 
 #taskset -c 4-7 box64 wine explorer /desktop=shell,800x600 $PREFIX/glibc/opt/autostart.bat &
 # Lançar o jogo com parâmetros otimizados BorderlandsGOTY.exe
-carregar_exports box64 wine explorer /desktop=shell,800x600  $PREFIX/glibc/opt/autostart.bat> "$HOME/box64.log" 2>&1 &
+
+(
+  carregar_exports 
+  box64 wine explorer /desktop=shell,800x600  $PREFIX/glibc/opt/autostart.bat
+)> "$HOME/box64.log" 2>&1 &
 #box64 wine /sdcard/Download/Jogos Winlator/Borderlands Game of the Year Enhanced/Binaries/Win64/BorderlandsGOTY.exe> "$HOME/box64.log" 2>&1 &
 
 #unset LD_PRELOAD;PATH="/data/data/com.termux/files/usr/glibc/bin:/data/data/com.termux/files/usr/bin" box64 wineboot --init &> "$HOME/box64.log" 2>&1 &
-carregar_exports
+
 #box64 wine "/sdcard/Download/Jogos Winlator/Borderlands Game of the Year Enhanced/Binaries/Win64/BorderlandsGOTY.exe"
